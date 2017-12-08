31bd216189e8f65c84b187d44dc37df21d7f2ff1
Gaohan <Gao@GaohandeMacBook-Pro.local>
2017-12-08 10:01:59 +0800

    分离之前的版本

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
-const router = require('koa-router');
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 
@@ -9,6 +10,8 @@ app.use(async (ctx, next) => {
     await next();
 })
 
+app.use(bodyParser());
+
 //add url-router
 router.get('/hello/:name', async(ctx, next) => {
     let name = ctx.params.name;
@@ -16,7 +19,25 @@ router.get('/hello/:name', async(ctx, next) => {
 })
 
 router.get('/', async(ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>`
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name : <input name="name" value="koa" /> </p>
+            <p>Password: <input name="password" type="password"/></p>
+            <p><input type="submit" value="Submit"/></p>
+        </form>`
+})
+
+router.post('/signin', async(ctx, next) => {
+    var name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`)
+    if (name === 'koa' && password === '123456') {
+        ctx.response.body = `<h1>Welcome, ${name}</h1>`
+    } else {
+        ctx.response.body = `<h1>Login failed</h1>
+        <p><a href="/">Try again</a></p>`
+        
+    }
 })
 
 //add router middleware
